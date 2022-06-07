55a34c1e9f4f47663a52a5da2b57bd56509fae86
imssiddiqui <106057264+imssiddiqui@users.noreply.github.com>
2022-06-07 10:51:34 +0530

    mongo session 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://functionupassignment:msJISmjxvX4gvZ9W@functionup.nyvlz.mongodb.net/booksmodel", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
